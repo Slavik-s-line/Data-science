51cbbdb29e291db6ea7276ff0e04577af7e82426
Viacheslav <78733510+Slavik-s-line@users.noreply.github.com>
2022-04-30 13:33:12 +0300

    Update hypothesis_2.py

--- hypothesis_2.py
+++ hypothesis_2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("investments_VC.csv")  # створюємо DataFrame на основі файлу з даними
 
@@ -9,6 +10,7 @@ def hypot_2():  # знаходимо країну з найб к-тью успі
     country = df[df["status"] == "acquired"]["country_code"].value_counts().index[0]
     print(f"Гіпотеза 2:\nНайбільше успішних стартапів в Китаї\n"
           f"ВИСНОВОК: Ні. Найбільше успішних стартапів в {country}\n")
-    
-    df[df["status"] == "acquired"]["country_code"].value_counts().head(5).plot(kind='pie')  # створення кругової діаграми
+
+    df[df["status"] == "acquired"]["country_code"].value_counts().head(5).plot(
+        kind='pie')  # створення кругової діаграми
     plt.show()
